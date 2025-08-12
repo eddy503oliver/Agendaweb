cee9855ac827ef5a0db3b093d1de133ec9584701
eddy503oliver <150090357+eddy503oliver@users.noreply.github.com>
2025-08-12 10:07:08 -0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/Agendaweb/', // 👈 Nombre exacto del repositorio
   server: {
     port: 3000,
     open: true
   }
-}) 
+})
